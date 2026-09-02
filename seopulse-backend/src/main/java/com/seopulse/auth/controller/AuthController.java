361f172f9299df42c639ae9ac3af15ebc294dfbc
Sunil Maurya <sunilmaurya7786@gmail.com>
2026-09-02 22:22:37 +0530

    add Registration and Login

---
+++ seopulse-backend/src/main/java/com/seopulse/auth/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.seopulse.auth.controller;
+
+import com.seopulse.auth.dto.AuthResponse;
+import com.seopulse.auth.dto.LoginRequest;
+import com.seopulse.auth.dto.RegisterRequest;
+import com.seopulse.auth.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(
+            @Valid @RequestBody RegisterRequest request
+    ) {
+        AuthResponse authResponse = authService.register(request);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(authResponse);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(
+            @Valid @RequestBody LoginRequest request
+    ) {
+        AuthResponse authResponse = authService.login(request);
+        return ResponseEntity.ok(authResponse);
+    }
+}
